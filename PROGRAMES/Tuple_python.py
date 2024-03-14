b5f98ae232d644b9bbd5b10e2d1a9197bbaca0eb
Rashid <sa7795815@gmail.com>
2024-03-14 06:43:54 +0500

    its all about tuple in pyhton..

---
+++ PROGRAMES/Tuple_python.py
@@ -0,0 +1,25 @@
+tuple1 = (12, 6, -8, "jenny", True, 12, 6)
+tuple2 = (45, 67, 90)
+# print(tuple2[1])
+# print(tuple2[-1])  # negative index always start from -1
+# tuple3 = (tuple1, tuple2)
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+# print(tuple3[1])
+# print(len(tuple3))
+
+print(min(tuple2))
+print(max(tuple2))
+print(tuple1.count(12))
+print(tuple1.index(12))
+List1 = [1, 2, 3, 4, 5]
+print(tuple(List1))  # convert list into tuple
+tuple4 = (10,)*3
+print(tuple4)
+# print(tuple1)
+# print(tuple2)
+# print(tuple2[::])
+# print(tuple2[::2])
+# print(len(tuple2))
+# print(type(tuple1))
+# print(type(tuple2))
