2d899481a73e85c6d75835b82a1a5099c9df50d0
Rashid <sa7795815@gmail.com>
2024-03-18 09:33:44 +0500

    its all about string in pyhton

---
+++ PROGRAMES/string3.py
@@ -0,0 +1,8 @@
+w = "WELCOME"
+print(w.isalpha())  # it mean all are alphabat then it become true
+
+w = "12345"
+print(w.isdigit())  # it means all are digites then it become true
+
+w = "welcome123"
+print(w.isalnum())  # if it is digit,string or mix of digit and string
